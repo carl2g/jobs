65f8ed61eb35a0cdb113677d3c45cbc7a31d5f77
Carl2g <degentile@gmail.com>
2022-03-10 13:25:07 +0100

    check reservation is in booking range

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,9 +2,16 @@ class Reservation < ApplicationRecord
   belongs_to :listing
 
   after_create :initialize_missions
+  validate :in_booking_range
 
   def initialize_missions
     self.listing.add_mission(Mission::CHECKOUT_CHECKIN, self.end_date)
+  end
+
+  def in_booking_range
+    if self.listing.bookings.where("start_date <= ? AND ? <= end_date ", self.start_date, self.end_date).count == 0
+      self.errors.add(:base, "Is not in any booking range")
+    end
   end
 
 end
